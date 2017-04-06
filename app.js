5a70bf250994694990c01b0733548eef0b4235e8
xuanminh12995 <minh.vu@u.nus.edu>
2017-04-06 16:47:49 +0800

    Check

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ function publish() {
   });
 }*/
 
+// Welcome to Johny
 // Johnny-Five
 // Using a temperature sensor, type LM35DZ
 // This requires OneWire support using the ConfigurableFirmata
@@ -42,64 +43,4 @@ tempController(app);
 //listen on port 3000
 app.listen(3000, function() {
     console.log("The frontend server is running on port 3000!");
-});
-
-var requirejs = require('requirejs');
-var sqlite3 = require('sqlite3').verbose();
-var db;
-var data;
-var dataArray=[];
-
-//create Database in sqlite and pushing data to it, then read it
-
-function createDb() {
-
-
-    db = new sqlite3.Database('temp.sqlite3', createTable);
-}
-
-function createTable() {
-
-    db.run("CREATE TABLE IF NOT EXISTS Temperature (temp FLOAT)", insertRows);
-}
-
-function insertRows(data) {
-    var data=temp;
-
-    var stmt = db.prepare("INSERT INTO Temperature VALUES (?)");
-
-
-    stmt.run(data);
-
-
-    stmt.finalize(readAllRows);
-}
-
-function readAllRows() {
-
-    db.all("SELECT rowid AS id, temp FROM Temperature", function(err, rows) {
-        rows.forEach(function (row) {
-            dataArray.push(row.temp)
-            console.log(dataArray);
-        });
-    });
-}
-
-
-var five = require('johnny-five');
-
-five.Board().on('ready', function() {
-  var temperature = new five.Thermometer({
-    controller: 'LM35',
-    pin: 0
-  });
-
-  temperature.on('data', function() {
-
-    //console.log('Address: 0x' + this.address.toString(16));
-    temp = this.celsius;
-    createDb();
-  });
-
-//  setInterval(publish, 3000);
 });
